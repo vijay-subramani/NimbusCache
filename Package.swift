b9f7e73838941fd863f6e657806464b3fa887bda
Vijay Subramani <vijaysubramani.s@caratlane.com>
2025-02-26 18:27:56 +0530

    Initial Commit

---
+++ Package.swift
@@ -0,0 +1,24 @@
+// swift-tools-version: 6.0
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "NimbusCache",
+    products: [
+        // Products define the executables and libraries a package produces, making them visible to other packages.
+        .library(
+            name: "NimbusCache",
+            targets: ["NimbusCache"]),
+    ],
+    targets: [
+        // Targets are the basic building blocks of a package, defining a module or a test suite.
+        // Targets can depend on other targets in this package and products from dependencies.
+        .target(
+            name: "NimbusCache"),
+        .testTarget(
+            name: "NimbusCacheTests",
+            dependencies: ["NimbusCache"]
+        ),
+    ]
+)
