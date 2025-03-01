3a6623cc9141d09bd3af60536f1900bb0b5de9c9
Vijay Subramani <vijaysubramani.ios.dev@gmail.com>
2025-03-01 19:53:02 +0530

    removed videoplayerviewmodel from example

--- Examples/NimbusCacheExample_SwiftUI/NimbusCacheExample_SwiftUI/VideoPlayerView.swift
+++ Examples/NimbusCacheExample_SwiftUI/NimbusCacheExample_SwiftUI/VideoPlayerView.swift
@@ -10,47 +10,8 @@ import SwiftUI
 import AVKit
 import NimbusCache
 
-class VideoPlayerViewModel: ObservableObject {
-    @Published var player: AVPlayer?
-    @Published var error: Error?
-
-    func loadAsset(from url: URL) {
-        let asset = AVURLAsset(url: url)
-
-        // Load the asset asynchronously
-//        asset.load(.tracks) { [weak self] in
-        asset.loadValuesAsynchronously(forKeys: ["tracks"]) { [weak self] in
-            guard let self = self else { return }
-
-            var error: NSError?
-//            let status = asset.status(of: .tracks)
-            let status = asset.statusOfValue(forKey: "tracks", error: &error)
-
-            DispatchQueue.main.async {
-                switch status {
-                case .loaded:
-                    // Asset is loaded, create the player item and player
-                    let playerItem = AVPlayerItem(asset: asset)
-                    self.player = AVPlayer(playerItem: playerItem)
-
-                case .failed:
-                    self.error = error
-                    print("Asset loading failed: \(error?.localizedDescription ?? "Unknown error")")
-
-                case .cancelled:
-                    print("Asset loading was cancelled")
-
-                default:
-                    break
-                }
-            }
-        }
-    }
-}
-
 struct VideoPlayerView: View {
     @State private var player = AVPlayer()
-    @StateObject private var viewModel = VideoPlayerViewModel()
     var videoUrl: URL?
 
     var body: some View {
