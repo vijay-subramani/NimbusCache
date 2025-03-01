0a6b6fde014f5bddcb3b45f93473b62f0d7420aa
Vijay Subramani <vijaysubramani.ios.dev@gmail.com>
2025-03-01 16:53:51 +0530

    package.swift update

--- Package.swift
+++ Package.swift
@@ -10,14 +10,13 @@ let package = Package(
         // Products define the executables and libraries a package produces, making them visible to other packages.
         .library(
             name: "NimbusCache",
-            targets: ["NimbusCache/Sources"]),
+            targets: ["NimbusCache"]),
     ],
     targets: [
         // Targets are the basic building blocks of a package, defining a module or a test suite.
         // Targets can depend on other targets in this package and products from dependencies.
         .target(
-            name: "NimbusCache",
-            path: "Sources"),
+            name: "NimbusCache"),
         .testTarget(
             name: "NimbusCacheTests",
             dependencies: ["NimbusCache"]
